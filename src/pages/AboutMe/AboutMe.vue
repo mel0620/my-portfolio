af8a316e4a9dfe7ebbb1d6b437abfdbdc8e297de
mel0620 <cunetarommel0620@gmail.com>
2019-10-14 18:16:59 +0800

    about

---
+++ src/pages/AboutMe/AboutMe.vue
@@ -0,0 +1,87 @@
+<template>
+    <section class="about-me-wrapper">
+        <div class="container">
+            <div data-scroll data-text="section-text-header" :style="`--text-bg: 'ABOUT ME'`">{{ section_text }}</div>
+            <div class="about-me" data-scroll>
+                <div class="portrait" :style="`background-image: url(${portrait})`">
+                    <!-- <img class="img-fluid" src="statics/me.png" alt="Rommel Cuneta's Photo"> -->
+                </div>
+                <div class="paragraph">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, eius dolorum? Dolorem, dignissimos deserunt. Et quam quas magnam, at cupiditate saepe dicta, atque minima tempore inventore dolore ut quaerat corporis.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet alias ipsam ullam illum architecto sit culpa quia. Consequatur, aliquam illo.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quae totam accusamus deserunt tenetur soluta, ipsam, esse, repudiandae temporibus beatae harum exercitationem? Magnam a facilis quibusdam molestiae recusandae dolorum sapiente.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import ScrollOut from "scroll-out";
+export default {
+    data () {
+        return {
+            section_text: 'About Me',
+            portrait: 'statics/me.png'
+        }
+    },
+    mounted () {
+        ScrollOut();
+    }
+}
+</script>
+
+<style lang="scss">
+.about-me-wrapper {
+    padding: 24px 0;
+    padding-bottom: 5rem;
+
+    .about-me {
+        padding-top: 3rem;
+        display: grid;
+        grid-template-columns: auto auto;
+        grid-gap: 2rem;
+
+        .portrait {
+            width: 400px;
+            height: 400px;
+            background-color: #eeeeee;
+            position: relative;
+            background-size: 350px;
+            background-position: 20px 20px;
+            background-repeat: no-repeat;
+            transition: .3s all ease-in-out;
+            z-index: 5;
+            clip-path: polygon(0 0, 95% 5%, 93% 93%, 0% 100%);
+            margin: auto;
+        }
+
+        .paragraph {
+            p {
+                letter-spacing: 1px;
+                font-weight: 400;
+            }
+        }
+
+        @media screen and (max-width: 500px) {
+            grid-template-columns: 1fr;
+
+            .portrait {
+                width: 300px;
+                height: 300px;
+                background-size: 250px;
+            }
+        }
+
+        @media screen and (max-width: 500px) {
+            grid-template-columns: 1fr;
+
+            .portrait {
+                width: 250px;
+                height: 250px;
+                background-size: 200px;
+            }
+        }
+    }
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,7 @@
 <template>
 	<q-page>
 		<splash-v2></splash-v2>
+		<AboutMe></AboutMe>
 		<technologies></technologies>
 	</q-page>
 </template>
@@ -8,12 +9,13 @@
 <script>
 const Splash = () => import(/*webpackChunkname: "splash"*/ "./Splash/Splash.vue");
 import SplashV2 from "./Splash/SplashV2.vue";
+import AboutMe from "./AboutMe/AboutMe.vue";
 import Technologies from "./Technologies/Technologies.vue";
 
 export default {
 	name: 'PageIndex',
 	components: {
-		Splash, SplashV2, Technologies
+		Splash, SplashV2, AboutMe ,Technologies
 	},
 	data () {
 		return {
