294c1834ef60fd7c3ee14360d4b1200ede3beb04
mel0620 <cunetarommel0620@gmail.com>
2019-10-15 15:39:30 +0800

    animation

--- src/pages/Home/Splash/SplashV2.vue
+++ src/pages/Home/Splash/SplashV2.vue
@@ -31,7 +31,7 @@ export default {
         const { title }     = this.$refs;
         const timeline 		= new TimelineLite()
 
-        timeline.to(slide, .8, {width: '100%', ease: Back.easeOut.config(1.7)})
+        timeline.to(slide, .6, {width: '100%'})
                 .to(slide, .6, {width: 0, right: 0, ease: Circ.easeOut})
                 .to(underline, .6, {width: '100%', opacity: .12})
                 .fromTo(name, .6, {y: 10, opacity: 0}, {y: 0, opacity: 1})
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,22 @@
 <template>
 	<q-page>
-		<splash-v2></splash-v2>
-		<AboutMe></AboutMe>
-		<technologies></technologies>
+		<SplashV2/>
+		<AboutMe/>
+		<Technologies/>
 	</q-page>
 </template>
 
 <script>
-const Splash = () => import(/*webpackChunkname: "splash"*/ "./Splash/Splash.vue");
-import SplashV2 from "./Splash/SplashV2.vue";
-import AboutMe from "./AboutMe/AboutMe.vue";
-import Technologies from "./Technologies/Technologies.vue";
+import SplashV2 from "./Home/Splash/SplashV2.vue";
+import AboutMe from "./Home/AboutMe/AboutMe.vue";
+import Technologies from "./Home/Technologies/Technologies.vue";
 
 export default {
 	name: 'PageIndex',
 	components: {
-		Splash, SplashV2, AboutMe ,Technologies
+		SplashV2, 
+		AboutMe,
+		Technologies
 	},
 	data () {
 		return {
